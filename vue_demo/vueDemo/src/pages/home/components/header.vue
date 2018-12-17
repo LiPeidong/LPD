6db0c9d1ef67762bdb87040c77e490f670bb8fc1
LPD <383776566@qq.com>
2018-12-17 16:49:17 +0800

    shangchuan

---
+++ vue_demo/vueDemo/src/pages/home/components/header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+        <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+        城市
+        <span class="iconfont">&#xe62d;</span>
+    </div>
+  </div>
+</template>
+<script>
+export default{
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .header{
+        display: flex
+        height: .86rem
+        background:$bgcolor
+        line-height: .86rem
+        color: #fff
+    }
+    .header .header-left{
+        width: .64rem
+        float: left
+    }
+    .header-left .back-icon{
+        margin-left:.1rem
+        font-size: .4rem
+    }
+    .header .header-input{
+        flex: 1
+        height: .64rem
+        margin-top: .12rem
+        margin-left: .2rem
+        background: #fff
+        border-radius: .1rem
+        color: #ccc
+        line-height: .64rem
+        padding-left: .2rem
+    }
+    .header .header-right{
+        width: 1.24rem
+        float: right
+        text-align:center
+    }
+</style>
---
+++ vue_demo/vueDemo/src/pages/home/components/swiper.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="wrapper">
+    <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" v-bind:key='item.id'>
+        <img class="swiper-img" v-bind:src="item.imgUrl"/>
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://attach.bbs.miui.com/forum/201303/16/173716jzszx8vbbb0z9o4k.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://imgsrc.baidu.com/imgad/pic/item/21a4462309f79052df1c9eea06f3d7ca7acbd5e7.jpg'
+      }
+      ]
+    }
+  }
+}
+console.log(document.getElementsByClassName('swiper-img'))
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active{
+        background: #fff
+    }
+    .swiper-img{
+        width: 100%;
+        height: 129px
+    }
+    .wrapper{
+        width: 100%
+        height:31.25vw
+        background: #eee
+    }
+</style>
